45dd39ffc0a3571b192bf89284b25b0b0b658c0d
Vivek M George <vivek.m.george@gmail.com>
2014-04-27 18:09:07 -0400

    completed homwork

--- week_4/4_nums_commas_solo_challenge/my_solution.rb
+++ week_4/4_nums_commas_solo_challenge/my_solution.rb
@@ -6,6 +6,14 @@
 
 
 # 1. Pseudocode
+# create a method called commas
+	# CASE number OF
+		#1000			:Print "1,000"
+		#1000000	:Print "1,000,000"
+		#10       :Print "10"
+		#1        :Print "1"
+	#ENDCASE
+
 
 # What is the input?
 # What is the output? (i.e. What should the code return?)
@@ -13,8 +21,9 @@
 
 
 # 2. Initial Solution
-
-
+def separate_comma(number)
+	number.to_s.gsub(/(\d{3})(?=\d)/, '\\1,')
+end
 
 
 # 3. Refactored Solution
@@ -22,3 +31,8 @@
 
 
 # 4. Reflection 
+# From earlier exercises I learned how to pass multiple arguments through one method so that was easier. The challenge was to write the actual code to put the commas in the correct place. I looked online and found some code on Stackoverflow to solve the problem. 
+
+# Doing these excercises have helped me clearly understand the difference between methods, arguments, elimates. 
+
+# I enjoyed getting the correct answer, but sometimes I didn't know what code to write to solve the problem and couldn't find anything via our resources or online. This I found extremely fraustrating and I am not sure what one does at that point.
