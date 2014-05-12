367a0aa8ffcd2e6e0ea5403fa89920b78bbfc109
Vivek M George <vivek.m.george@gmail.com>
2014-05-11 21:37:27 -0400

    not completed but attempted

--- week_6/2_drawer_debugger/my_solution.rb
+++ week_6/2_drawer_debugger/my_solution.rb
@@ -8,86 +8,88 @@
 
 class Drawer
 
-attr_reader :contents
+	attr_reader :contents
 
 # Are there any more methods needed in this class?
 
-def initialize
-@contents = []
-@open = true
-end
+	def initialize
+		@contents = []
+		@open = true
+	end
 
-def open
-@open = true
-end
+	def open
+		@open = true
+	end
 
-def close
-@open = false
-end 
+	def close
+		@open = false
+	end 
 
-def add_item
-@contents << item
-end
+	def add_item
+		@contents << item
+	end
 
-def remove_item(item = @contents.pop) #what is `#pop` doing?
-@contents.delete(item)
-end
+	def remove_item(item = @contents.pop) #what is `#pop` doing?
+		@contents.delete(item)
+	end
 
-def dump  # what should this method return?
-puts "Your drawer is empty."
-end
+	def dump  # what should this method return?
+		puts "Your drawer is empty."
+	end
 
-def view_contents
-puts "The drawer contains:"
-@contents.each {|silverware| puts "- " + silverware.type }
-end
+	def view_contents
+		puts "The drawer contains:"
+		@contents.each {|silverware| puts "- " + silverware.type }
+	end
 
 
 class Silverware
-attr_reader :type
+	attr_reader :type
 
-# Are there any more methods needed in this class?
+	# Are there any more methods needed in this class?
 
-def initialize(type, clean = true)
-@type = type
-@clean = clean
+	def initialize(type, clean = true)
+		@type = type
+		@clean = clean
+	end
+
+	def eat
+		puts "eating with the #{type}"
+		@clean = false
+	end
+
 end
-
-def eat
-puts "eating with the #{type}"
-@clean = false
-end
-
 end
 
 knife1 = Silverware.new("knife")
 
-silverware_drawer = Drawer.new
-silverware_drawer.add_item(knife1) 
-silverware_drawer.add_item(Silverware.new("spoon"))
-silverware_drawer.add_item(Silverware.new("fork")) 
-silverware_drawer.view_contents
+# silverware_drawer = Drawer.new
+# silverware_drawer.add_item(Silverware.new("knife")
+# silverware_drawer.add_item(knife1)Silverware.new("knife") 
+# silverware_drawer.add_item(Silverware.new("spoon"))
+# silverware_drawer.add_item(Silverware.new("fork")) 
+# silverware_drawer.view_contents
 
-silverware_drawer.remove_item
-silverware_drawer.view_contents
-sharp_knife = Silverware.new("sharp_knife")
-
-
-silverware_drawer.add_item(sharp_knife)
-
-silverware_drawer.view_contents
-
-removed_knife = silverware_drawer.remove_item(sharp_knife)
-removed_knife.eat
-removed_knife.clean_silverware 
-
-silverware_drawer.view_contents
-silverware_drawer.dump
-silverware_drawer.view_contents #What should this return?
+# silverware_drawer.remove_item
+# silverware_drawer.view_contents
+# sharp_knife = Silverware.new("sharp_knife")
 
 
-fork = silverware_drawer.remove_item(fork) #add some puts statements to help you trace through the code...
-fork.eat
+# silverware_drawer.add_item(sharp_knife)
+
+# silverware_drawer.view_contents
+
+# removed_knife = silverware_drawer.remove_item(sharp_knife)
+# removed_knife.eat
+# removed_knife.clean_silverware 
+
+# silverware_drawer.view_contents
+# silverware_drawer.dump
+# silverware_drawer.view_contents #What should this return?
+
+
+# fork = silverware_drawer.remove_item(fork) #add some puts statements to help you trace through the code...
+# fork.eat
 
 #BONUS SECTION
 # puts fork.clean
@@ -99,4 +101,4 @@ fork.eat
 
 
 
-# 5. Reflection 
+# 5. Reflection
